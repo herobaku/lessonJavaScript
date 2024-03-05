19e7efd4a6a12c6e3f66f6f31a86ff5d18143da9
herobaku <mammedovemin@gmail.com>
2024-03-05 23:41:17 +0400

    update 2

--- Javascript-3/main.js
+++ Javascript-3/main.js
@@ -33,3 +33,59 @@
 //   return one
 // }
 // console.log(double([1, 2, 5, 10]))
+// Task 04
+// function fruits(arr) {
+//   let result = "";
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     result += `${arr[i]} `
+//   }
+//   return result;
+// }
+// let switchFruits = fruits(["apple", "orange", "banana"]);
+// console.log(switchFruits);
+// Task 5
+// function plusMinus() {
+//   const user = +prompt("Reqem daxil et")
+//   if (user > 0) {
+//     console.log("Pozitiv")
+//   } else if (user < 0) {
+//     console.log("Neqativ")
+//   } else {
+//     console.log("zero")
+//   }
+// }
+// plusMinus();
+// Task 6
+// const hello = () => "Hello";
+// const world = () => "World";
+// function helloWorld() {
+//   console.log(hello() + " " + world())
+// }
+// helloWorld();
+// Task 07
+// function eded() {
+// 3 * 4 = 12; 12 / 2 = 6
+//   let arr = [4, 5]
+//   let result = 1;
+//   for (let i = 0; i <= arr.length - 1; i++) {
+//     result *= arr[i];
+//   }
+//   console.log(result / arr.length)
+// }
+// eded();
+// function ed(a, b) {
+//   return (a * b) / 2;
+// }
+// console.log( ed(5,10) )
+// Task 9
+// function cem(a, b, c) {
+//   let result = "";
+//   return result
+// }
+// console.log(cem(1, 5, 10));
+// function control(a, b, c) {
+//   if ((b > a && b < c) || (b < a && b > c)) {
+//     return `${b} ${a} ${c}`
+//   }
+// }
+// console.log(control(45, 30, 15));
